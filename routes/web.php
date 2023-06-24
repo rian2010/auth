dff011ef02e132a97a53a076e22107455e0734f7
rian2010 <riaansensei@gmail.com>
2023-06-24 23:21:55 +0700

    remove some comment

--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,7 @@ use Inertia\Inertia;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+
 
 Route::get('/', [LandingpageController::class, 'index'])->name('landingpage');
 Route::get('/company', [CompanyViewController::class, 'index'])->name('company');
